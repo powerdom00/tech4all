4257a5bd1bcde84d17b71c0a8d6fc5444b9950ce
Marco Capuano <marcocapuano2000@gmail.com>
2024-12-16 13:05:25 +0100

    Aggiunta variabili d'ambiente back-end e fix package
    Implementazione delle variabili d'ambiente per il back-end, modificato il file di configurazione del database e i file package per migliore compatibilità dei componenti.

--- server/src/config/db.config.ts
+++ server/src/config/db.config.ts
@@ -1,6 +1,33 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "1212",
-  DB: "tech4all",
+  HOST: process.env.DB_HOST || "",
+  USER: process.env.DB_USER || "",
+  PASSWORD: process.env.DB_PASSWORD || "",
+  DB: process.env.DB_NAME || "",
 };
+
+// Controllo delle variabili di ambiente
+if (!process.env.DB_HOST) {
+  console.error(
+    "Inserire la variabile d'ambiente DB_HOST in un .env nella cartella server.",
+  );
+  process.exit(1);
+} else if (!process.env.DB_USER) {
+  console.error(
+    "Inserire la variabile d'ambiente DB_USER in un .env nella cartella server.",
+  );
+  process.exit(1);
+} else if (!process.env.DB_PASSWORD) {
+  console.error(
+    "Inserire la variabile d'ambiente DB_PASSWORD in un .env nella cartella server.",
+  );
+  process.exit(1);
+} else if (!process.env.DB_NAME) {
+  console.error(
+    "Inserire la variabile d'ambiente DB_NAME in un .env nella cartella server.",
+  );
+  process.exit(1);
+}
